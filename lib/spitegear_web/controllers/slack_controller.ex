a5a96a49963a3121c4212d9fe510bde21749c7f4
Adam Richard <arichard44@gmail.com>
2024-07-27 18:05:12 -0500

    auto start game poller

--- lib/spitegear_web/controllers/slack_controller.ex
+++ lib/spitegear_web/controllers/slack_controller.ex
@@ -10,15 +10,30 @@ defmodule SpitegearWeb.SlackController do
   def handle_events(conn, %{"type" => "event_callback", "event" => event}) do
     Logger.info("Received event: #{inspect(event)}")
 
-    # DynamicSupervisor.start_child(
-    #   GameSupervisor,
-    #   Spitegear.Worker.GamePoller.child_spec(game_id: "81437099")
-    # )
+    case extract_game_id(event) do
+      {:ok, game_id} -> start_child(game_id)
+      _ -> nil
+    end
 
     send_resp(conn, 200, "ok")
   end
 
   def handle_events(conn, _params) do
     send_resp(conn, 400, "Bad Request")
+  end
+
+  defp start_child(game_id) do
+    DynamicSupervisor.start_child(
+      GameSupervisor,
+      Spitegear.Worker.GamePoller.child_spec(game_id: game_id)
+    )
+  end
+
+  @game_url_pattern ~r|wargear\.net/games/join/(\d+)|
+  defp extract_game_id(%{"text" => text}) do
+    case Regex.run(@game_url_pattern, text) do
+      [_, game_id] -> {:ok, game_id}
+      _ -> :error
+    end
   end
 end
